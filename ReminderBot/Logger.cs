c3c8367449cbab9dd4297ebe7f5db7a31e017a60
AntitheticallyAnonymous <anthonymuy@gmail.com>
2017-08-07 16:48:29 -0500

    Refactoring. Added credentials.json

---
+++ ReminderBot/Logger.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReminderBot
+{
+    class Logger
+    {
+        /* 
+        * Logs information to console 
+        * Code taken from Discord.Net sample code
+        */
+        public Task Log(LogMessage msg)
+        {
+            var cc = Console.ForegroundColor;
+            switch (msg.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{msg.Severity,8}] {msg.Source}: {msg.Message}");
+            Console.ForegroundColor = cc;
+
+            return Task.CompletedTask;
+        }
+    }
+}
