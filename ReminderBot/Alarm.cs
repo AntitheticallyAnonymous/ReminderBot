3efbf1e886c103445201135c4c966e32cc065f48
AntitheticallyAnonymous <anthonymuy@gmail.com>
2017-09-11 00:39:58 -0500

    Added Alarm + Builder Class. Parsing of user input. Added testing libraries.

---
+++ ReminderBot/Alarm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReminderBot
+{
+    class Alarm
+    {
+        public ulong alarmId { get; set; }
+        public string when { get; set; }
+        public string message { get; set; }
+        public ulong userId { get; set; }
+        public ulong channelId { get; set; }
+        public int interval { get; set; }
+        public int repeat { get; set; }
+        public bool started { get; set; }
+
+        public Alarm(AlarmBuilder a)
+        {            
+            if (String.IsNullOrEmpty(a.when))
+            {
+                throw new ArgumentNullException("Alarm cannot be set to a null/empty time. Check the construction of the alarm.");
+            }
+            if (a.message == null)
+            {
+                message = "";
+            }
+
+            alarmId = a.alarmId;
+            when = a.when;
+            message = a.message;
+            userId = a.userId;
+            channelId = a.channelId;
+            interval = a.interval;
+            repeat = a.repeat;
+            started = false;            
+        }
+    }
+}
